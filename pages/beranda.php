f47233f927146edba9a40bbce9fdba1f9469ba54
ihsbramn <abdurahmanihsan25@gmail.com>
2021-10-21 15:18:00 +0700

    update

--- pages/beranda.php
+++ pages/beranda.php
@@ -1,9 +1,8 @@
 <div id="label-page"><h3>Beranda</h3></div>
 <div id="content">
 	<div id="beranda-judul">
-		<h1>SELAMAT DATANG DI SISTEM INFORMASI PERPUSTAKAAN TELKOM</h1>
+		<h1>Dashboard Data Rental Pakaian Telyu</h1>
 	</div>
 	<div id="beranda-konten">
-		<h2> "MEMBACA ADALAH JENDELA DUNIA" </h2>
-	</div>
+		</div>
 </div>
